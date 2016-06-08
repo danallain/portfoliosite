d7e246b4e500d57a794a4dc30bb18c61810ea10a
Daniel Allain <dan.allain@gmail.com>
2016-06-08 02:55:35 -0300

    Initial Commit

---
+++ READMNE.md
@@ -0,0 +1,1 @@
+"# This is my portfolio site to showcase my work and work experience." 
---
+++ plugins/pico_edit-master/config.php
@@ -0,0 +1,11 @@
+<?php
+
+global $backend_password;
+
+/*
+ * This should be a sha1 hash of your password.
+ * Use a tool like http://www.sha1-online.com to generate.
+ */
+$backend_password = '';
+
+?>
